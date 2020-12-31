9f31aa2b127655e958f767d49a4db533ea49e2d1
Najwanyusnianda <najwanyusnianda@gmail.com>
2020-12-31 14:07:15 +0700

    init

---
+++ src/components/GuestList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="table w-full">
+            <div class="table-row">
+                <div class="table-cell">Nama Lengkap</div>
+            </div>
+            <div class="table-row" v-for="customer in customers" :key="customer.id">
+                 <div class="table-cell font-bold">{{customer.nama_depan}} {{customer.nama_belakang}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'GuestList',
+    data(){
+        return {
+            customers:[
+                {
+                    id:1,
+                    nama_depan: 'Najwan',
+                    nama_belakang: ' Yusnianda',
+
+                }
+            ]
+        }
+    },
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+
+    <guest-list></guest-list>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+//import GuestList from '@/components/GuestList.vue'
+import GuestList from '../components/GuestList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    GuestList
+  
+  }
+}
+</script>
