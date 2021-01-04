5948a8c8ae1422ab7f534ec7cc7c019c9c31c1ad
najwanyusnianda <najwanyusnianda@gmail.com>
2021-01-04 23:35:54 +0700

    wow

---
+++ src/components/GuestItem.vue
@@ -0,0 +1,33 @@
+<template>
+    
+        
+    <tr class="hover:bg-grey-lighter border-b border-1 ">
+        <td class="py-4 px-6 ">
+            {{customer.nama_depan}} {{customer.nama_belakang}}
+        </td>
+        <td>
+            {{customer.instansi}}
+        </td>
+        <td>
+            <a href="#" class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark">Edit</a>
+            <a href="#" class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">View</a>
+        </td>
+    </tr>
+    
+
+</template>
+
+<script>
+export default {
+    name:'guest-item',
+    props:{
+        customer:{
+            Object:{
+                type:Object,
+                required:true
+            }
+        }
+
+    }
+}
+</script>
--- src/components/GuestList.vue
+++ src/components/GuestList.vue
@@ -1,19 +1,38 @@
 <template>
-    <div>
-        <div class="table w-full">
-            <div class="table-row">
-                <div class="table-cell">Nama Lengkap</div>
-            </div>
-            <div class="table-row" v-for="customer in customers" :key="customer.id">
-                 <div class="table-cell font-bold">{{customer.nama_depan}} {{customer.nama_belakang}}</div>
-            </div>
-        </div>
+    <div class="w-2/3 mx-auto">
+<div class="bg-white shadow-md rounded my-5">
+    <table class="w-full text-left">
+        <thead>
+            <tr class="bg-gray-50" >
+                <th class="text-gray-600 py-4 px-6 border-light-gray  border-2 font-bold ">
+                    Nama Lengkap
+                </th>
+                <th class="py-4 px-6 border-light-grey  border-2 font-bold ">
+                    Instansti
+                </th>
+                <th class="py-4 px-6 border-light-grey  border-2 font-bold ">
+                    Status
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <guest-item v-for="(customer) in customers" :key="customer.id" :customer="customer" >
+
+            </guest-item>
+        </tbody>
+    </table>
+</div>
     </div>
 </template>
 
 <script>
+import GuestItem from '../components/GuestItem'
+
 export default {
     name:'GuestList',
+    components:{
+        GuestItem
+    },
     data(){
         return {
             customers:[
@@ -21,8 +40,17 @@ export default {
                     id:1,
                     nama_depan: 'Najwan',
                     nama_belakang: ' Yusnianda',
-
-                }
+                    instansi: 'Badan Pusat Statistik Kabupaten Aceh Singkil',
+                    status:'menunggu',
+                    
+                },
+                {
+                    id:2,
+                    nama_depan: 'Najwan2',
+                    nama_belakang: ' Yusnianda',
+                    instansi: 'Badan Pusat Statistik Kabupaten Aceh Singkil',
+                    status:'menunggu',
+                }  
             ]
         }
     },
